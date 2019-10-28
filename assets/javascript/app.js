d2fe705cb85957a07e57c1b7dcb2dd49d3aad382
Maddie <54539182+maddieclare@users.noreply.github.com>
2019-10-28 21:17:15 +0800

    Added functionality to load more GIFs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,6 +19,10 @@ let searchTerm;
 
 let queryUrl;
 
+let lastTopic;
+
+let timesLoadMoreClicked = 1;
+
 function refreshTopicButtons() {
   $("#search-buttons").empty();
 
@@ -32,6 +36,8 @@ function refreshTopicButtons() {
 }
 
 function loadGifs(whichTopic) {
+    $("#results").empty();
+  lastTopic = $(whichTopic);
   searchTerm = $(whichTopic).attr("id");
   queryUrl =
     "https://api.giphy.com/v1/gifs/search?q=" +
@@ -79,4 +85,51 @@ function startStopGifs(whichImage) {
     $(whichImage).attr("src", $(whichImage).attr("data-still"));
     $(whichImage).attr("data-state", "still");
   }
+}
+
+function showLoadMoreButton() {
+  $("#load-more").css("display", "block");
+}
+
+function loadMoreGifs(whichTopic) {
+  timesLoadMoreClicked + 1;
+  console.log(timesLoadMoreClicked);
+  whichTopic = lastTopic;
+  searchTerm = $(whichTopic).attr("id");
+  queryUrl =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    searchTerm +
+    "&api_key=jhPOZhUKBDuDczyOSuEabgqPr58TyBMk&limit=10&offset=" +
+    10 * timesLoadMoreClicked;
+
+  $.ajax({
+    url: queryUrl,
+    method: "GET"
+  }).then(function(response) {
+    let results = response.data;
+
+    for (let i = 0; i < results.length; i++) {
+      let gifDiv = $("<div>");
+      gifDiv.attr("class", "image-result rounded");
+
+      let rating = results[i].rating;
+
+      let info = $("<div>");
+      info.attr("class", "image-details");
+      info.html("<p> Rating: " + rating.toUpperCase() + "</p>");
+
+      image = $("<img>");
+      image.attr("src", results[i].images.fixed_height_still.url);
+      image.attr("data-still", results[i].images.fixed_height_still.url);
+      image.attr("data-animate", results[i].images.fixed_height.url);
+      image.attr("data-state", "still");
+      image.attr("class", "image-only");
+      image.attr("id", i);
+
+      gifDiv.prepend(info);
+      gifDiv.prepend(image);
+
+      $("#results").append(gifDiv);
+    }
+  });
 }
